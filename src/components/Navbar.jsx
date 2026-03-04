96a5e799eb51500cd3a8a90eb7a3d0ab1df2f6f7
SamChan123 <samuelchandra2022@gmail.com>
2026-03-04 13:11:42 +0700

    Mobile Responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,7 +60,7 @@ function Navbar() {
 
       <aside 
         className={`fixed top-0 left-0 h-screen w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 lg:hidden ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
+          isOpen ? "translate-x-0" : "-translate-x-full" //Conditional Rendering.
         }`}
       >
         <div className="flex flex-col pt-20 px-6 space-y-4">
